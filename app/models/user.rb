fd636d35f21b9a424b34df443b4f67c03a56f9cf
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-03-11 16:24:07 +0900

    Add: rails db:migrateを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :email, presence: true, uniqueness: true
+
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
