061fc528942f2fe9cd60e8b9789fe329ee7595b7
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-04-02 17:29:17 +0900

    Add: routeを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :articles, dependent: :destroy
+  
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :email, presence: true, uniqueness: true
 
