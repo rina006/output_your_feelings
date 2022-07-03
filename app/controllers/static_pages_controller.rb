c68dbba009dd8b05b34b436ec477a0db619ef5d0
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-07-03 16:08:01 +0900

    Add: 利用規約ページを作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
   def top; end
+
+  def terms; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  get 'terms', to: 'static_pages#terms'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
