c2f818bcdedd8ddfca3531c53f3d24133c4f6f54
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-07-04 10:15:50 +0900

    Add: プライバシーポリシーを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,6 @@ class StaticPagesController < ApplicationController
   def top; end
 
   def terms; end
+
+  def privacy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
 
   get 'terms', to: 'static_pages#terms'
+  get "privacy", to: "static_pages#privacy"
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
