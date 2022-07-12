824afaa51e723506a04fa67216fa6ae2bafb2477
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-07-12 22:41:03 +0900

    Add: Userモデルにadmin判定用のカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
+
+  enum role: { general: 0, admin: 1 }
 end
