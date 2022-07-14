76ad490aa7fab45741d56bf67ec235f0fd8cc339
rina006 <ag1d.rina_m1512@i.softbank.jp>
2022-07-14 19:56:11 +0900

    Update: application_helper作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'MyEmotion',
+      title: 'MyEmotion',
+      reverse: true,
+      charset: 'utf-8',
+      description: '通常は紙とペンを用いて行う筆記開示をWeb上で行うサービスです',
+      keywords: '筆記開示, Expressive Writing, ストレス軽減, リラックス, スッキリ',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: [
+          '@__rina06',
+        ]
+      }
+    }
+  end
 end
